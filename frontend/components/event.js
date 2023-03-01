2ef2dc90aa532727778f60d91ecc5debffe128bc
Thomas McCoy <finnmccoy99@gmail.com>
2023-03-01 15:46:09 +0000

    Event component displays price range

--- frontend/components/event.js
+++ frontend/components/event.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default function Event({ eventName }) {
+export default function Event({ eventData }) {
+  const { eventName, priceRanges } = eventData;
   return (
     <View>
       <Text>{eventName}</Text>
+      <Text>{priceRanges}</Text>
     </View>
   );
 }
-
-//Mocking the event data from the backend and then calling them in the previous function
--- frontend/components/event.test.js
+++ frontend/components/event.test.js
@@ -3,8 +3,18 @@ import Event from './event';
 
 describe('Event component testing', () => {
   test('it displays the name of the event', () => {
-    const { getByText } = render(<Event eventName='Dirty DnB Fridays!' />);
+    const { getByText } = render(
+      <Event eventData={{ eventName: 'Dirty DnB Fridays!' }} />
+    );
     const textElement = getByText('Dirty DnB Fridays!');
+    expect(textElement).toBeDefined();
+  });
+
+  test('it displays the Price Range if present', () => {
+    const { getByText } = render(
+      <Event eventData={{ priceRanges: '£22 - £30 ' }} />
+    );
+    const textElement = getByText('£22 - £30 ');
     expect(textElement).toBeDefined();
   });
 });
