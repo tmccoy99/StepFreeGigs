0f1da906ee0dc49ee32b3ec547cf2d9c1f9c0a18
Tom Rolfe <tomrolfe20@gmail.com>
2023-03-01 16:10:27 +0000

    added keys and files

---
+++ backend/clients/TFLClient.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const tflAPIKey = require('../tfl-api-key');
+const tflAPIId = require('../tfl-api-id');
+
+class TFLCLient {
+  constructor() {
+    this.baseUrl = 'https://api.tfl.gov.uk/journey/journeyresults';
+  }
+
+  async getDirections(start, destination) {
+    const response = await axios.get(
+      `${this.baseUrl}/${start}/to/${destination}?accessibilityPreference=StepFreeToVehicle&app_key=${tflAPIKey}&app_id=${tflAPIId}`
+    );
+
+    const directions = {
+      journeys: response.journeys,
+      lines: response.lines,
+    };
+
+    return directions;
+  }
+  catch(error) {
+    console.error(error);
+  }
+}
+
+// const client = new TFLCLient();
+// console.log(client.getDirections('SW99QH', 'SW99SL'));
+
+module.exports = TFLCLient;
