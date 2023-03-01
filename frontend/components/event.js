e33f80d52cf2f4e79d3821ad9a55a967bf88b082
Thomas McCoy <finnmccoy99@gmail.com>
2023-03-01 15:54:17 +0000

    Event component displays the venue

--- frontend/components/event.js
+++ frontend/components/event.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function Event({ eventData }) {
-  const { eventName, priceRanges } = eventData;
+  const { eventName, priceRanges, venue } = eventData;
   return (
     <View>
       <Text>{eventName}</Text>
       <Text>{priceRanges}</Text>
+      {/* <Text>{date}</Text> */}
+      <Text>{venue}</Text>
     </View>
   );
 }
--- frontend/components/event.test.js
+++ frontend/components/event.test.js
@@ -17,4 +17,18 @@ describe('Event component testing', () => {
     const textElement = getByText('£22 - £30 ');
     expect(textElement).toBeDefined();
   });
+
+  test('it displays the venue', () => {
+    const { getByText } = render(<Event eventData={{ venue: 'The Garage' }} />);
+    const textElement = getByText('The Garage');
+    expect(textElement).toBeDefined();
+  });
+
+  // test('it displays the Date', () => {
+  //   const { getByText } = render(
+  //     <Event eventData={{ date: '2023-03-01T19:00:00Z' }} />
+  //   );
+  //   const textElement = getByText('2023-03-01T19:00:00Z');
+  //   expect(textElement).toBeDefined();
+  // });
 });
