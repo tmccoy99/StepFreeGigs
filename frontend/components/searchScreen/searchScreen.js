a73d345bfd0f6f3e963e3233aed693fbd44977d0
santosluizfelipe <luizfelipe0598@icloud.com>
2023-03-02 14:55:55 +0000

    Tested and implemented simple funcionality for Find events near me button

--- frontend/components/searchScreen/searchScreen.js
+++ frontend/components/searchScreen/searchScreen.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import { Button, Text, View, Image } from 'react-native';
-//import Geolocation from '@react-native-community/geolocation';
+// import Geolocation from '@react-native-community/geolocation';
+const baseURL = process.env.NATIVE_APP_API_URL
 
 
 export default function SearchScreen() {
+
+  onPress = () => {
+    fetch(`${baseURL}/events`)
+  }
+
   return (
     <>
-      <Button title="Find events near me!" />
+      <Button onPress={onPress} title="Find events near me!" />
       <Image testID="logo" source={require('../../assets/stepfreegigs-logo.png')}></Image>
       <Text>StepFreeGigs</Text>
     </>
--- frontend/components/searchScreen/searchScreen.test.js
+++ frontend/components/searchScreen/searchScreen.test.js
@@ -1,7 +1,14 @@
 import { render, fireEvent } from '@testing-library/react-native';
 import SearchScreen from './searchScreen';
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
+const baseURL = process.env.NATIVE_APP_API_URL
 
 describe('SearchScreen component testing', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+
   it('Before location entered, displays the name of the app', () => {
     const { getByText } = render(<SearchScreen />);
     const textElement = getByText('StepFreeGigs');
@@ -15,8 +22,14 @@ describe('SearchScreen component testing', () => {
   })
 
   it('Should render button named "Find events near me!"', () => {
-    const {getByText} = render(<SearchScreen/>)
+    const { getByText } = render(<SearchScreen/>)
     const eventsButton = getByText('Find events near me!')
     expect(eventsButton).toBeDefined()
+  })
+
+  it('Pressing events button should make a GET request to the /events', () => {
+    const { getByText } = render(<SearchScreen/>)
+    fireEvent.press(getByText('Find events near me!'))
+    expect(fetch.mock.calls[0][0]).toBe(`${baseURL}/events`)
   })
 });
