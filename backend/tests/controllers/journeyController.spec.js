a827688c69b4bad0c3071aaef4f3267b0a20dcd6
Joe Burgess <josephburgess@gmail.com>
2023-03-02 16:13:01 +0000

    adding params for start and destination

--- backend/tests/controllers/journeyController.spec.js
+++ backend/tests/controllers/journeyController.spec.js
@@ -1,11 +1,27 @@
-const app = require('../../app')
-const request = require('supertest')
+const app = require('../../app');
+const request = require('supertest');
 
 describe('journeyController', () => {
   describe('without any query parameters', () => {
     it('should respond with status 400', async () => {
-      response = await request(app).get('/events')
-      expect(response.status).toEqual(400)
+      response = await request(app).get('/journey');
+      expect(response.status).toEqual(400);
+      expect(response.text).toContain('Bad request');
+    });
+    it('should respond with status 400 for missing start postcode', async () => {
+      response = await request(app)
+        .get('/journey')
+        .query({ destination: 'SW1A 2AA' });
+
+      expect(response.status).toEqual(400);
+      expect(response.text).toContain('Bad request');
+    });
+
+    it('should respond with status 400 for missing destination postcode', async () => {
+      response = await request(app).get('/journey').query({ start: 'NW1 5LA' });
+
+      expect(response.status).toEqual(400);
+      expect(response.text).toContain('Bad request');
     });
   });
 });
