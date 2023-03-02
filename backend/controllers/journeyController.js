9e49a3ccb32b381fde12153ce96ac3c60a6e002d
Joe Burgess <josephburgess@gmail.com>
2023-03-02 16:13:01 +0000

    adding params for start and destination

--- backend/controllers/journeyController.js
+++ backend/controllers/journeyController.js
@@ -1,9 +1,13 @@
+const TFLClient = require('../clients/TFLClient.js');
+
 const JourneyController = {
-  Index: (req, res) => {
-    
-    res.status(400)
-    res.send('return the events near the user')
-  }
-}
+  Index: async (req, res) => {
+    const { start, destination } = req.query;
+    if (!start || !destination) {
+      res.status(400).send('Bad request');
+      return;
+    }
+  },
+};
 
 module.exports = JourneyController;
