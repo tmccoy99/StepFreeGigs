ed7356a85ee24d9d1555cd7da6065ef79cd51f95
Joe Burgess <josephburgess@gmail.com>
2023-03-02 17:21:00 +0000

    removing postcode regex erroneously left in code

--- backend/controllers/journeyController.js
+++ backend/controllers/journeyController.js
@@ -3,7 +3,6 @@ const TFLClient = require('../clients/TFLClient.js');
 const JourneyController = {
   Index: async (req, res) => {
     try {
-      const postcodeRegex = /^[A-Za-z]{1,2}\d{1,2}[A-Za-z]?\s?\d[A-Za-z]{2}$/;
       const { start, destination } = req.query;
       if (!start || !destination) {
         res.status(400).send('Bad request');
