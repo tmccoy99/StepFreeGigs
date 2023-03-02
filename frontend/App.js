c211bfa1005a1e06f32ea0a9419be4304857a1d7
Tom Rolfe <tomrolfe20@gmail.com>
2023-03-02 15:37:21 +0000

    added mock leg testing

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function App() {
+  Geolocation.getCurrentPosition((info) => console.log(info));
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
--- frontend/components/journeyScreen/journeyScreen.js
+++ frontend/components/journeyScreen/journeyScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Text, View, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
-export default function JourneyScreen({navigation}) {
-  return(
+export default function JourneyScreen({
+  navigation,
+  startLocation,
+  endLocation,
+}) {
+  useEffect(() => {
+    const getDirections = async (startLocation, endLocation) => {
+      const result = await axios.get();
+      return result;
+    };
+
+    startLocation && endLocation
+      ? getDirections(startLocation, endLocation)
+      : null;
+  }, [startLocation, endLocation]);
+
+  return (
     <>
       <Text>Directions</Text>
     </>
-  )
+  );
 }
