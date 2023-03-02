b22e20fd4d4477187ac2146cb88bccffe9c7c376
Joe Burgess <josephburgess@gmail.com>
2023-03-02 11:40:46 +0000

    fixing tests

--- backend/tests/clients/googlePlacesClient.spec.js
+++ backend/tests/clients/googlePlacesClient.spec.js
@@ -6,10 +6,15 @@ jest.mock('axios');
 describe('Google places Client', () => {
   it('getPlaceId returns a place id', async () => {
     const mockResponse = {
-      candidates: [
-        { name: 'O2 Academy Brixton', place_id: 'ChIJL1UqD2cEdkgRidF8HHIq4fQ' },
-      ],
-      status: 'OK',
+      data: {
+        candidates: [
+          {
+            name: 'O2 Academy Brixton',
+            place_id: 'ChIJL1UqD2cEdkgRidF8HHIq4fQ',
+          },
+        ],
+        status: 'OK',
+      },
     };
     axios.get.mockResolvedValue(mockResponse);
 
@@ -22,13 +27,15 @@ describe('Google places Client', () => {
   });
   it('getPlaceId returns a place id with mispelt venue + capital letters', async () => {
     const mockResponse = {
-      candidates: [
-        {
-          name: 'O2 Academy Brixton',
-          place_id: 'ChIJL1UqD2cEdkgRidF8HHIq4fQ',
-        },
-      ],
-      status: 'OK',
+      data: {
+        candidates: [
+          {
+            name: 'O2 Academy Brixton',
+            place_id: 'ChIJL1UqD2cEdkgRidF8HHIq4fQ',
+          },
+        ],
+        status: 'OK',
+      },
     };
     axios.get.mockResolvedValue(mockResponse);
 
@@ -54,47 +61,49 @@ describe('Google places Client', () => {
   it('getVenueDetails returns a postcode and wheel chair accessible boolean is true', async () => {
     const mockResponse = {
       html_attributions: [],
-      result: {
-        address_components: [
-          {
-            long_name: '211',
-            short_name: '211',
-            types: ['street_number'],
-          },
-          {
-            long_name: 'Stockwell Road',
-            short_name: 'Stockwell Rd',
-            types: ['route'],
-          },
-          {
-            long_name: 'London',
-            short_name: 'London',
-            types: ['postal_town'],
-          },
-          {
-            long_name: 'Greater London',
-            short_name: 'Greater London',
-            types: ['administrative_area_level_2', 'political'],
-          },
-          {
-            long_name: 'England',
-            short_name: 'England',
-            types: ['administrative_area_level_1', 'political'],
-          },
-          {
-            long_name: 'United Kingdom',
-            short_name: 'GB',
-            types: ['country', 'political'],
-          },
-          {
-            long_name: 'SW9 9SL',
-            short_name: 'SW9 9SL',
-            types: ['postal_code'],
-          },
-        ],
-        wheelchair_accessible_entrance: true,
+      data: {
+        result: {
+          address_components: [
+            {
+              long_name: '211',
+              short_name: '211',
+              types: ['street_number'],
+            },
+            {
+              long_name: 'Stockwell Road',
+              short_name: 'Stockwell Rd',
+              types: ['route'],
+            },
+            {
+              long_name: 'London',
+              short_name: 'London',
+              types: ['postal_town'],
+            },
+            {
+              long_name: 'Greater London',
+              short_name: 'Greater London',
+              types: ['administrative_area_level_2', 'political'],
+            },
+            {
+              long_name: 'England',
+              short_name: 'England',
+              types: ['administrative_area_level_1', 'political'],
+            },
+            {
+              long_name: 'United Kingdom',
+              short_name: 'GB',
+              types: ['country', 'political'],
+            },
+            {
+              long_name: 'SW9 9SL',
+              short_name: 'SW9 9SL',
+              types: ['postal_code'],
+            },
+          ],
+          wheelchair_accessible_entrance: true,
+        },
+        status: 'OK',
       },
-      status: 'OK',
     };
     axios.get.mockResolvedValue(mockResponse);
 
