2d4a0e1a095bcd977682c36b4dda89e7975ac1c5
Joe Burgess <josephburgess@gmail.com>
2023-03-02 14:46:54 +0000

    feat: controller now grabs accessible events and outputs in an array

--- backend/controllers/eventsController.js
+++ backend/controllers/eventsController.js
@@ -12,16 +12,29 @@ const EventsController = {
       const client = new TicketMasterClient();
       const googleClient = new GooglePlacesClient();
       const events = await client.getEvents(latlong, radius);
-      const event = events[0];
-      const venueName = event.venue;
+      const accessibleEvents = [];
 
-      const placeId = await googleClient.getPlaceId(venueName);
-      if (placeId) {
-        const venueDetails = await googleClient.getVenueDetails(
-          placeId.placeId
-        );
+      for (let i = 0; i < events.length; i++) {
+        const event = events[i];
+        const venueName = event.venue;
+
+        const placeId = await googleClient.getPlaceId(venueName);
+        if (placeId) {
+          const venueDetails = await googleClient.getVenueDetails(
+            placeId.placeId
+          );
+
+          if (venueDetails && venueDetails.wheelchair_accessible_entrance) {
+            accessibleEvents.push(event);
+          }
+        }
+
+        if (accessibleEvents.length === 5) {
+          break;
+        }
       }
-      res.status(200).send('OK');
+
+      res.status(200).json({ accessibleEvents });
       return;
     } catch (error) {
       console.error(error);
