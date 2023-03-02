174f1dedee96a9e5e5698883a86e12ec0ef09de6
Joe Burgess <josephburgess@gmail.com>
2023-03-02 16:20:07 +0000

    adding postcode check

--- backend/controllers/journeyController.js
+++ backend/controllers/journeyController.js
@@ -3,8 +3,12 @@ const TFLClient = require('../clients/TFLClient.js');
 const JourneyController = {
   Index: async (req, res) => {
     const { start, destination } = req.query;
+    const postcodeRegex = /^[A-Za-z]{1,2}\d{1,2}[A-Za-z]?\s?\d[A-Za-z]{2}$/;
     if (!start || !destination) {
       res.status(400).send('Bad request');
+      return;
+    } else if (!postcodeRegex.test(start) || !postcodeRegex.test(destination)) {
+      res.status(400).send('Invalid postcode format');
       return;
     }
   },
