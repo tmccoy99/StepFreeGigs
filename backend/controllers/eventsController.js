b746711243de58bcc8d73ae0b795412abe57cede
Joe Burgess <josephburgess@gmail.com>
2023-03-02 14:53:26 +0000

    refactoring controller

--- backend/controllers/eventsController.js
+++ backend/controllers/eventsController.js
@@ -12,35 +12,30 @@ const EventsController = {
       const client = new TicketMasterClient();
       const googleClient = new GooglePlacesClient();
       const events = await client.getEvents(latlong, radius);
-      const accessibleEvents = [];
-
-      for (let i = 0; i < events.length; i++) {
-        const event = events[i];
-        const venueName = event.venue;
-
-        const placeId = await googleClient.getPlaceId(venueName);
-        if (placeId) {
-          const venueDetails = await googleClient.getVenueDetails(
-            placeId.placeId
-          );
-
-          if (venueDetails && venueDetails.wheelchair_accessible_entrance) {
-            accessibleEvents.push(event);
-          }
-        }
-
-        if (accessibleEvents.length === 5) {
-          break;
-        }
-      }
+      const accessibleEvents = await getAccessibleEvents(events, googleClient);
 
       res.status(200).json({ accessibleEvents });
-      return;
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal server error');
     }
   },
 };
+
+async function getAccessibleEvents(events, googleClient) {
+  const accessibleEvents = [];
+  for (let i = 0; i < events.length && accessibleEvents.length < 5; i++) {
+    const event = events[i];
+    const venueName = event.venue;
+    const placeId = await googleClient.getPlaceId(venueName);
+    if (placeId) {
+      const venueDetails = await googleClient.getVenueDetails(placeId.placeId);
+      if (venueDetails && venueDetails.wheelchair_accessible_entrance) {
+        accessibleEvents.push(event);
+      }
+    }
+  }
+  return accessibleEvents;
+}
 
 module.exports = EventsController;
