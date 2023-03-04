a3492de88bf5ba6b62b97cd728fff77c1a6d6829
alexHoggett <alexmh99@gmail.com>
2023-03-04 19:56:04 +0000

    styled the event component

--- frontend/components/event/event.js
+++ frontend/components/event/event.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Button, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Button, Text, View, TouchableOpacity } from 'react-native';
+import Collapsible from 'react-native-collapsible';
 
 export default function Event({ eventData, navigate }) {
   const { eventName, priceRanges, venue, url, distance } = eventData;
@@ -10,15 +11,21 @@ export default function Event({ eventData, navigate }) {
   };
 
   return (
-    <View>
+    <View style={styles.eventContainer}>    
       <TouchableOpacity onPress={onPress} testID='eventButton'>
-        <Text>{eventName}</Text>
-        <Text>{priceRanges}</Text>
-        {/* <Text>{date}</Text> */}
-        <Text>{distance}</Text>
-        <Text>{venue}</Text>
-        {isExpanded && (
-          <>
+        <View style={styles.eventHeader}>
+          <View style={styles.eventHeaderLeft}>
+            <Text style={styles.eventName}>{eventName}</Text>
+            <Text style={styles.eventVenue}>{venue}</Text>
+          </View>
+          {/* <Text>{date}</Text> */}
+          <View style={styles.eventHeaderRight}>
+            <Text style={styles.eventPrice}>{priceRanges}</Text>
+            <Text style={styles.eventDistance}>{distance}</Text>
+          </View>
+        </View>
+        <Collapsible collapsed={!isExpanded}>
+          <View style={styles.eventFooter}>
             <Button
               title='Buy Tickets'
               onPress={() => {
@@ -30,10 +37,54 @@ export default function Event({ eventData, navigate }) {
               onPress={() => {
                 navigate('/journey');
               }}
-            />{' '}
-          </>
-        )}
+            />
+          </View>
+        </Collapsible>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  eventContainer: {
+    backgroundColor: '#D3D3D3',
+    padding: 15,
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 20,
+  },
+  eventHeader: {
+    minHeight: 50,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  eventHeaderLeft: {
+
+  },
+  eventHeaderRight: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  eventPrice: {
+    
+  },
+  eventName: {
+    fontWeight: 700,
+    fontSize: 18,
+    marginBottom: 8
+  },
+  eventDistance: {
+
+  },
+  eventVenue: {
+    fontSize: 16
+  },
+  eventFooter: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: -8
+  },
+})
--- frontend/components/searchScreen/searchScreen.js
+++ frontend/components/searchScreen/searchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Text, View, Image } from 'react-native';
+import { StyleSheet, Button, Text, View, Image } from 'react-native';
 import axios from 'axios';
 import Event from '../event/event';
 import logo from '../../assets/stepfreegigs-logo.png';
@@ -8,11 +8,22 @@ const baseURL = 'http://localhost:3000';
 export default function SearchScreen() {
   const [events, setEvents] = useState(null);
   const onPress = async () => {
-    const eventsData = await axios.get(`http://localhost:3000/events`, {
-      params: { latlong: '51.4919120,-0.1692555', radius: '5' },
-    });
-    setEvents(eventsData.accessibleEvents);
+    const eventsData = await axios.get(`${baseURL}/events`);
+    setEvents(eventsData);
   };
+  
+  const testData = {
+    id: '1',
+    image: 'www.imageurl.com',
+    eventName: 'Event 1',
+    url: 'https://www.ticketmaster.com/event1',
+    date: '2023-03-01T19:00:00Z',
+    distance: '1.5 miles',
+    priceRanges: '£10 - £40',
+    venue: 'Venue 1',
+    venueAddress: '123 Main St',
+    venuePostcode: '12345',
+  }
 
   return (
     <View testID='SearchScreen'>
@@ -21,16 +32,25 @@ export default function SearchScreen() {
         title='Find events near me!'
         testID='search-button'
       />
+
+      <Event eventData={testData} />
+
       {events &&
         events.map((data, index) => (
           <Event eventData={data} key={index} testID='Event' />
         ))}
       {!events && (
         <>
-          <Text>StepFreeGigs</Text>
-          <Image testID='logo' source={logo} />
+          <Image testID='logo' source={logo} style={styles.logo} />
         </>
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 400,
+    height: 400
+  },
+})
