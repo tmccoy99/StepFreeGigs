82cc05dc5b2bea96748679d34d8fc8f0a76ccc97
Joe Burgess <josephburgess@gmail.com>
2023-03-04 16:46:41 +0000

    amending styling and adding mapLayout usestate hook

--- frontend/components/map/map.js
+++ frontend/components/map/map.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 
 export default RouteMap = ({ legs }) => {
-  const region = {
-    latitude: legs[0].departurePoint.lat,
-    longitude: legs[0].departurePoint.lon,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
+  const [mapLayout, setMapLayout] = useState(null);
+
+  const onMapLayout = (event) => {
+    setMapLayout(event.nativeEvent.layout);
   };
 
   const markers = legs.map((leg, index) => (
@@ -22,22 +21,56 @@ export default RouteMap = ({ legs }) => {
     />
   ));
 
-  const polylines = legs.map((leg, index) => (
-    <Polyline
-      key={index}
-      coordinates={JSON.parse(leg.path.lineString)}
-      strokeColor='#F00'
-      strokeWidth={3}
-      testID='polyline'
-    />
-  ));
+  const polylines = legs.map((leg, index) => {
+    const polylineCoords = JSON.parse(leg.path.lineString);
+    const polylineObjects = polylineCoords.map((coord) => ({
+      latitude: coord[0],
+      longitude: coord[1],
+    }));
+
+    return (
+      <Polyline
+        key={index}
+        coordinates={polylineObjects}
+        strokeColor='#F00'
+        strokeWidth={3}
+        testID='polyline'
+      />
+    );
+  });
+
+  let region = null;
+  if (mapLayout) {
+    region = {
+      latitude: legs[0].departurePoint.lat,
+      longitude: legs[0].departurePoint.lon,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    };
+  }
 
   return (
-    <View>
-      <MapView region={region} testID='map-view'>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={region}
+        onLayout={onMapLayout}
+        testID='map-view'
+      >
         {markers}
         {polylines}
       </MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
