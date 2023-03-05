f3d49ecbcc909ac191d1d9de75f4ddf62e05aace
Thomas McCoy <finnmccoy99@gmail.com>
2023-03-05 15:50:50 +0000

    Changed tests to ensure that testing occurs once useEffect has concluded

--- frontend/components/journeyScreen/journeyScreen.spec.js
+++ frontend/components/journeyScreen/journeyScreen.spec.js
@@ -5,30 +5,49 @@ import { mockTFLResponse } from '../../fixtures/mockTFLResponse';
 jest.mock('axios');
 
 describe('Journey Screen component testing', () => {
-  test('displays Steps button', () => {
-    const renderedComponent = render(<JourneyScreen />);
+  let renderedComponent;
+  beforeEach(async () => {
+    mockAxios.get.mockResolvedValueOnce(mockTFLResponse);
+  });
+
+  test('displays Steps button', async () => {
+    await waitFor(() => {
+      renderedComponent = render(
+        <JourneyScreen startLocation={'SW99QH'} endLocation={'SW99SL'} />
+      );
+    });
     expect(renderedComponent.queryByText('Steps')).not.toBeNull();
   });
 
-  test('displays Map button', () => {
-    const renderedComponent = render(<JourneyScreen />);
+  test('displays Map button', async () => {
+    await waitFor(() => {
+      renderedComponent = render(
+        <JourneyScreen startLocation={'SW99QH'} endLocation={'SW99SL'} />
+      );
+    });
     expect(renderedComponent.queryByText('Map')).not.toBeNull();
   });
 
-  test('Map component not displayed before Map button pressed', () => {
-    const renderedComponent = render(<JourneyScreen />);
+  test('Map component not displayed before Map button pressed', async () => {
+    await waitFor(() => {
+      renderedComponent = render(
+        <JourneyScreen startLocation={'SW99QH'} endLocation={'SW99SL'} />
+      );
+    });
     expect(renderedComponent.queryByTestId('Map')).toBeNull();
   });
 
-  test('Map component displayed after Map button pressed', () => {
-    const renderedComponent = render(<JourneyScreen />);
+  test('Map component displayed after Map button pressed', async () => {
+    await waitFor(() => {
+      renderedComponent = render(
+        <JourneyScreen startLocation={'SW99QH'} endLocation={'SW99SL'} />
+      );
+    });
     fireEvent.press(renderedComponent.getByText('Map'));
     expect(renderedComponent.queryByTestId('Map')).not.toBeNull();
   });
 
-  test('renders journey components once data fetched ', async () => {
-    mockAxios.get.mockResolvedValueOnce(mockTFLResponse);
-    let renderedComponent;
+  test('renders journey components initially', async () => {
     await waitFor(() => {
       renderedComponent = render(
         <JourneyScreen startLocation={'SW99QH'} endLocation={'SW99SL'} />
