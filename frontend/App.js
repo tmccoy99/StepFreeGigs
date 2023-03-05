94031b601ffbea3aef72226f210a1c4635969d08
Thomas McCoy <finnmccoy99@gmail.com>
2023-03-05 11:57:38 +0000

    Set up navigation in root component

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchScreen from './components/searchScreen/searchScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SearchScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Search' component={SearchScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
