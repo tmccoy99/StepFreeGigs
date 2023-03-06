3a7812b9eaada923316f365db5c55058ef35a55b
Tom Rolfe <tomrolfe20@gmail.com>
2023-03-06 14:26:50 +0000

    re created pull request

---
+++ frontend/components/journeyScreen/journeyScreen.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import axios from 'axios';
+
+export default function JourneyScreen({
+  navigation,
+  startLocation,
+  endLocation,
+}) {
+  const [directions, setDirections] = useState(null);
+  const [displayType, setDisplayType] = useState('Steps');
+  const viewMap = () => {
+    setDisplayType('Map');
+  };
+  const viewSteps = () => {
+    setDisplayType('Steps');
+  };
+
+  useEffect(() => {
+    const getDirections = async () => {
+      const result = await axios.get('http://localhost:3000/');
+      setDirections(result);
+    };
+    if (!directions) getDirections();
+  }, []);
+
+  return (
+    <>
+      <View>
+        <Button
+          title='Steps'
+          onPress={viewSteps}
+          testID='Steps button'
+        ></Button>
+        <Button title='Map' onPress={viewMap} testID='Map button'></Button>
+      </View>
+      <View>
+        {displayType === 'Steps' ? (
+          directions &&
+          directions.journeys.map((steps, index) => (
+            <Text testID='Step' key={`Journey-${index}`}></Text>
+          ))
+        ) : (
+          <Text testID='Map' />
+        )}
+      </View>
+    </>
+  );
+}
