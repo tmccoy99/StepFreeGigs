ffa2b98c357d88f7aa4669550e4de500f8ae27d8
Thomas McCoy <finnmccoy99@gmail.com>
2023-03-06 14:07:43 +0000

    Tested and implemented alert for invalid URL

--- frontend/components/buyTicketsButton/buyTicketsButton.test.js
+++ frontend/components/buyTicketsButton/buyTicketsButton.test.js
@@ -1,13 +1,6 @@
 import { render, fireEvent, waitFor } from '@testing-library/react-native';
 import BuyTicketsButton from './buyTicketsButton';
-import 'react-native';
-
-jest.mock('react-native', () => ({
-  ...jest.requireActual('react-native'),
-  Alert: {
-    alert: jest.fn(),
-  },
-}));
+import { Alert } from 'react-native';
 
 describe('BuyTicketsButton component testing', () => {
   test('button renders with correct text', () => {
@@ -16,10 +9,12 @@ describe('BuyTicketsButton component testing', () => {
   });
 
   test('if button is given invalid url as prop, pressing button raises alert', async () => {
+    const alertMock = jest.spyOn(Alert, 'alert');
     const renderedButton = render(<BuyTicketsButton url='123' />);
     await waitFor(() => {
       fireEvent.press(renderedButton.getByTestId('Buy Tickets button'));
     });
-    expect(Alert.alert).toHaveBeenCalledWith('Cannot open URL: 123');
+    expect(alertMock).toHaveBeenCalledWith('Cannot open URL: 123');
+    alertMock.mockRestore();
   });
 });
