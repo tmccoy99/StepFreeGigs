c5984748482de436357a1259983c1871b447a8c9
Thomas McCoy <finnmccoy99@gmail.com>
2023-03-06 12:00:02 +0000

    Setup testing of raising alert

--- frontend/components/buyTicketsButton/buyTicketsButton.js
+++ frontend/components/buyTicketsButton/buyTicketsButton.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, TouchableWithoutFeedback, Alert } from 'react-native';
 
-export default function BuyTicketsButton() {
+export default function BuyTicketsButton({ url }) {
+  const handlePress = () => {
+    Alert.alert(`Cannot open URL: ${url}`);
+  };
+
   return (
-    <View>
-      <Text>Buy Tickets</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress} testID='Buy Tickets button'>
+      <View>
+        <Text>Buy Tickets</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
--- frontend/components/buyTicketsButton/buyTicketsButton.test.js
+++ frontend/components/buyTicketsButton/buyTicketsButton.test.js
@@ -1,9 +1,25 @@
-import { render } from '@testing-library/react-native';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
 import BuyTicketsButton from './buyTicketsButton';
+import 'react-native';
+
+jest.mock('react-native', () => ({
+  ...jest.requireActual('react-native'),
+  Alert: {
+    alert: jest.fn(),
+  },
+}));
 
 describe('BuyTicketsButton component testing', () => {
   test('button renders with correct text', () => {
     const renderedButton = render(<BuyTicketsButton />);
     expect(renderedButton.queryByText('Buy Tickets')).not.toBeNull();
+  });
+
+  test('if button is given invalid url as prop, pressing button raises alert', async () => {
+    const renderedButton = render(<BuyTicketsButton url='123' />);
+    await waitFor(() => {
+      fireEvent.press(renderedButton.getByTestId('Buy Tickets button'));
+    });
+    expect(Alert.alert).toHaveBeenCalledWith('Cannot open URL: 123');
   });
 });
