d2f5a47ed09c2ecf5966c39bf7739890ab123e5b
santosluizfelipe <luizfelipe0598@icloud.com>
2023-03-08 15:45:58 +0000

    fiex white tone in the app.js component

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import SearchScreen from './components/searchScreen/searchScreen';
 import JourneyScreen from './components/journeyScreen/journeyScreen';
@@ -39,23 +39,42 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Search'>
+
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator 
+          initialRouteName='Search'
+          screenOptions={{
+            contentStyle: {
+              backgroundColor: '#FFFFFF',
+            },
+          }}
+        >
         <Stack.Screen
           name='Search'
           component={SearchScreen}
           initialParams={{ currentLocation }}
+          options={{
+            headerStyle: {
+              backgroundColor: '#FFA458',
+            },
+            headerTintColor: '#FFA458',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            // additional styles for the SearchScreen component can go here
+          }}
         />
         <Stack.Screen name='Journey' component={JourneyScreen}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
